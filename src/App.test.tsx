e7ddac63eef6266758b88a2cbf2176db45234356
Simon Kerr <simon.p.kerr@gmail.com>
2021-10-26 13:40:49 +0100

    initial commit. set up app. add test stubs for api

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render the correct components', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Star wars characters')).toBeInTheDocument();
 });
---
+++ src/api/api.test.ts
@@ -0,0 +1,12 @@
+import api from '.';
+
+jest.mock('axios');
+describe('API', () => {
+  describe('findCharacters', () => {
+    it('should call the correct endpoint and return characters', () => {});
+
+    it('should accept a page number', () => {});
+
+    it('should accept a search parameter', () => {});
+  });
+});
